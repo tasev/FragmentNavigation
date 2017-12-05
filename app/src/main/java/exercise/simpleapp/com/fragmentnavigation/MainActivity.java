673e769dca9ede41f600978f9c0e554bcff64ddd
tasev <tase.tasev@gmail.com>
2017-12-05 19:28:48 +0100

    4 Fragments added

--- app/src/main/java/exercise/simpleapp/com/fragmentnavigation/MainActivity.java
+++ app/src/main/java/exercise/simpleapp/com/fragmentnavigation/MainActivity.java
@@ -28,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
     public void setUpViewPager(ViewPager upViewPager) {
         MyPageAdapter adapter = new MyPageAdapter(this.getSupportFragmentManager());
         adapter.addFragment(new Fragment1(), "tab 1");
+        adapter.addFragment(new Fragment2(), "tab 2");
+        adapter.addFragment(new Fragment3(), "tab 3");
+        adapter.addFragment(new Fragment4(), "tab 4");
         pager.setAdapter(adapter);
     }
 }
